e80fb11f6ea4c9d2fac241deac982588e41b76dd
micos7 <micos7@gmail.com>
2017-04-12 12:59:00 +0300

    Form

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,26 @@
+@extends('main')
+
+@section('title', '| Create new post')
+
+@section('content')
+
+<div class="row">
+  <div class="col-md-8 col-md-offset-2">
+    <h1>Create New Post</h1>
+    <hr>
+    <form method="POST" action="{{ route('posts.store') }}">
+      <div class="form-group">
+        <label name="title">Title:</label>
+        <input id="title" name="title" class="form-control">
+      </div>
+      <div class="form-group">
+        <label name="body">Post Body:</label>
+        <textarea id="body" name="body" rows="10" class="form-control"></textarea>
+      </div>
+      <input type="submit" value="Create Post" class="btn btn-success btn-lg btn-block">
+      {{ csrf_field() }}
+    </form>
+  </div>
+</div>﻿
+
+@endsection
