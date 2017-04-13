158f13c4fdb2bee4f2ea92809d09ae3d46b34399
micos7 <micos7@gmail.com>
2017-04-13 16:26:10 +0300

    Dates and Edit buttons

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,9 +3,40 @@
 @section('title','| View post')
 
 @section('content')
+<div class="row">
+  <div class="col-md-8">
 
-<h1>{{ $post->title }}</h1>
+    <h1>{{ $post->title }}</h1>
 
-<p class="lead">{{ $post->body}}</p>
+    <p class="lead">{{ $post->body}}</p>
+    </div>
+    <div class="col-md-4">
+      <div class="well">
+        <dl class="dl-horizontal">
+          <dt>Created at:</dt>
+          <dd>{{date('M j, Y H:i',strtotime($post->created_at))}}</dd>
+        </dl>
+
+        <dl class="dl-horizontal">
+          <dt>Last updated:</dt>
+          <dd>{{date('M j, Y H:i',strtotime($post->updated_at))}}</dd>
+        </dl>
+        <hr>
+
+        <div class="row">
+          <div class="col-sm-6">
+
+             <a href="{{  route('posts.edit', $post->id, 'Edit')  }}" class="btn btn-primary btn-block">Edit</a>
+          </div>
+          <div class="col-sm-6">
+             <a href="{{  route('posts.destroy', $post->id, 'Delete')  }}" class="btn btn-danger btn-block">Delete</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+</div>
+
 
 @endsection
